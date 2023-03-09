ea3a568c1aa09ab191b66ac9c3012496e050cbcf
Koshoeva Ajar <ajarka1334@gmail.com>
2023-03-09 15:43:25 +0100

    added photo for my code from nasa

--- script.js
+++ script.js
@@ -24,4 +24,29 @@ fetch('http://www.floatrates.com/daily/rub.json')
     .then((response) => response.json())
     .then(data => {
         kgsToRub.textContent = data.kgs.rate.toFixed(2) + "com";
-    });    
+    });
+
+const weather = document.querySelector('#weather');
+fetch('https://api.open-meteo.com/v1/forecast?latitude=42.4907&longitude=78.3936&current_weather=true')
+    .then((response) => response.json())
+    .then(data => {
+        weather.textContent = data.current_weather.temperature + "°C";
+    });
+
+const nasaPhoto = document.querySelector('#nasa-photo');
+fetch('https://go-apod.herokuapp.com/apod')
+    .then((response) => response.json())
+    .then(data => {
+        nasaPhoto.textContent = "";
+
+        const img = document.createElement('img');
+        img.src = data.url;
+        img.alt = data.title;
+        img.width = 300;
+        nasaPhoto.append(img);
+    });
+
+
+
+
+
